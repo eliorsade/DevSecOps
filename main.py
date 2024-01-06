5cb22b70b5638a6baec1a488b535479fa3e1c0fb
eliorsade <108821327+eliorsade@users.noreply.github.com>
2024-01-06 12:45:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -77,9 +77,9 @@ for region in all_regions:
                 if any(ip_range.get('CidrIp') == '0.0.0.0/0' for ip_range in rule.get('IpRanges', [])):
                     with open(filename, "a") as logfile:
                         logfile.write("Region: " + region + " The SG: " + sg['GroupName'] + " " + sg['GroupId'] + " has inbound rule from the world\n")
-                if not log_mode:
-                    print("Deleting rules")
-                    client.revoke_security_group_ingress(GroupId=sg['GroupId'], IpPermissions=[rule])
+                    if not log_mode:
+                        print("Deleting rules", sg['GroupName'], sg['GroupId'], rule)
+                        client_region.revoke_security_group_ingress(GroupId=sg['GroupId'], IpPermissions=[rule])
 
     except ClientError as error_region:
         print("In region", region, "There is an error:",  error_region)
